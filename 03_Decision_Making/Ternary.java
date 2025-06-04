d6b990481cd99c35d5f195d2b1690dd1cc9db59e
ErGovindthakur <ergovindthakur@gmail.com>
2025-06-04 14:17:56 +0530

    solve nested ternary operator problem

---
+++ 03_Decision_Making/Ternary.java
@@ -0,0 +1,10 @@
+
+public class Ternary {
+  public static void main(String[] args) {
+     int num = 12;
+
+     String result = num<0? "Negative Num" : num>0 ? "Positive num" : "Zero";
+
+     System.out.println(result);
+  }   
+}

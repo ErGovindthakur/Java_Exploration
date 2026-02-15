cee92be722bc3020f8762d922fce69af5e2e2835
ErGovindthakur <ergovindthakur@gmail.com>
2026-02-15 12:14:49 +0530

    explore boolean data types and relational ops

---
+++ DSA_MASTER_IN_JAVA/Basic_Of_DSA/RelationalOps.java
@@ -0,0 +1,20 @@
+package DSA_MASTER_IN_JAVA.Basic_Of_DSA;
+
+public class RelationalOps {
+     public static void main(String[] args) {
+          // ==, !=, <,>, =<, <=, !
+
+          System.out.println(6==5);
+          System.out.println(6<5);
+          System.out.println(6>5);
+          System.out.println(6<=5);
+          System.out.println(6>=5);
+          System.out.println(6!=5);
+
+          // Boolean operator
+          boolean flag = true;
+          boolean flag2 = false;
+          System.out.println(flag2+" and "+flag);
+          System.out.println(!flag2+" and "+flag);
+     }
+}

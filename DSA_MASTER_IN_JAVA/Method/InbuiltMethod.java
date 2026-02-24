23e95d4de24960a4e75e5a94c4b0e8633cb36bda
ErGovindthakur <ergovindthakur@gmail.com>
2026-02-24 23:36:27 +0530

    explore most used Math method in java

---
+++ DSA_MASTER_IN_JAVA/Method/InbuiltMethod.java
@@ -0,0 +1,19 @@
+package DSA_MASTER_IN_JAVA.Method;
+
+public class InbuiltMethod {
+     public static void main(String[] args) {
+          // Explore most used inbuilt Math method in java
+
+          System.out.println(Math.PI); // it's a value of math
+          System.out.println(Math.abs(-12)); // it's a math method gives us +ve value even given -ve value
+          System.out.println(Math.cbrt(9)); // it gives us cube root
+          System.out.println(Math.ceil(9.9)); // it gives greatest ceil value
+          System.out.println(Math.ceil(-9.9)); // it gives greatest ceil value
+          System.out.println(Math.floor(-4.5)); // it gives greatest integer value
+          System.out.println(Math.max(12,12.4));
+          System.out.println(Math.min(12,12.4));
+          System.out.println(Math.pow(3, 2));
+          System.out.println(Math.random());
+          System.out.println(Math.round(3.4));
+     }
+}

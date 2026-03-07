f6cbea14cb2f77b29f36472c39b40e83f4682ed0
ErGovindthakur <ergovindthakur@gmail.com>
2026-03-07 16:48:27 +0530

    solve bst time to buy and sell stock problem

---
+++ DSA_MASTER_IN_JAVA/Array/ArrayLeetCode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,22 @@
+package DSA_MASTER_IN_JAVA.Array.ArrayLeetCode;
+
+public class BestTimeToBuyAndSellStock {
+     static int bstTmToBuyAndSell(int[]arr){
+          int min = arr[0]; // buy time
+          int maxProfit = 0;
+
+          // since we are supposing here 1st day is buying day
+          for(int i = 1; i<arr.length; i++){
+               if(arr[i]<min){
+                    min = arr[i];
+               }
+               int profit = arr[i]-min;
+               maxProfit = Math.max(maxProfit, profit);
+          }
+          return maxProfit;
+     }
+    public static void main(String[] args) {
+     int[]arr = {4,1,7,9,2};
+     System.out.println("Max Profit : "+ bstTmToBuyAndSell(arr));
+    } 
+}

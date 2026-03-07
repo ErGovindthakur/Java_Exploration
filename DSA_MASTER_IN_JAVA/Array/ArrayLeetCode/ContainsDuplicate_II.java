37a320c60d3b6d64ff36c1e0d833697564954d49
ErGovindthakur <ergovindthakur@gmail.com>
2026-03-08 00:18:46 +0530

    solve contains duplicate elem 2 via using sliding window and hasSet ds

---
+++ DSA_MASTER_IN_JAVA/Array/ArrayLeetCode/ContainsDuplicate_II.java
@@ -0,0 +1,27 @@
+package DSA_MASTER_IN_JAVA.Array.ArrayLeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate_II {
+     static boolean containsDup(int[]nums, int k){
+
+          Set<Integer> st = new HashSet<>();
+
+          for(int i=0; i<nums.length; i++){
+               if(st.contains(nums[i])){
+                    return true;
+               }
+               st.add(nums[i]);
+
+               if(st.size()>k)st.remove(nums[i-k]);
+               System.out.println(st.toString());
+          }
+          return false;
+     }
+    public static void main(String[] args) {
+     int [] nums = {1,3,2,4,1};
+     int k = 3;
+     System.out.println("Contains Dup: "+containsDup(nums, k));
+    } 
+}

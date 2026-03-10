ce9be321f185d6f6daca12c14a7c1cc0de9d2f6f
ErGovindthakur <ergovindthakur@gmail.com>
2026-03-10 17:50:43 +0530

    solve twosum with dup array problem

---
+++ DSA_MASTER_IN_JAVA/DSA_PATTERN_WISE/TwoPointer/TwoSumWithDupElem.java
@@ -0,0 +1,45 @@
+package DSA_MASTER_IN_JAVA.DSA_PATTERN_WISE.TwoPointer;
+
+import java.util.ArrayList;
+
+public class TwoSumWithDupElem {
+     static ArrayList<Integer> twoSumWithDup(int[] arr, int target) {
+          ArrayList<Integer> arl = new ArrayList<>();
+
+          int st = 0;
+          int end = arr.length - 1;
+
+          while (st < end) {
+               if (arr[st] + arr[end] == target) {
+                    arl.add(arr[st]);
+                    arl.add(arr[end]);
+                    st++;
+                    end--;
+                    if (arr[st] == arr[st + 1] && arr[end] == arr[end - 1]) {
+                         st++;
+                         end--;
+
+                    } else if (arr[st] == arr[st + 1]) {
+                         st++;
+                    } else if (arr[end] == arr[end - 1]) {
+                         end--;
+                    }
+               } else if (arr[st] + arr[end] > target) {
+                    end--;
+               } else {
+                    st++;
+               }
+               // st++;
+               // end--;
+          }
+          return arl;
+     }
+
+     public static void main(String[] args) {
+          int[] arr = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7 };
+          int target = 7;
+
+          System.out.println(twoSumWithDup(arr, target));
+
+     }
+}
